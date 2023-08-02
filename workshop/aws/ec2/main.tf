e220b119a53b93906973a7839654ec50e6eefb59
Achim Staebler <astaebler@splunk.com>
2023-08-02 15:37:02 +0200

    don't use ! for password

--- workshop/aws/ec2/main.tf
+++ workshop/aws/ec2/main.tf
@@ -173,7 +173,7 @@ resource "aws_route" "o11y-ws-route" {
 
 resource "random_string" "password" {
   length           = 16
-  override_special = "_%@$#!"
+  override_special = "_%@$#"
 }
 
 locals {
